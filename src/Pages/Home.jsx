80aa4e7077b9a282cfde6ae76b1e143ec249c063
viktordoms <viktorrap2@gmail.com>
2022-04-10 20:36:56 +0300

    Change Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 
-import Testpage from './Testpage'
-import axiosInstance from '../axios'
+import HomeSlider from "../Components/Home_slider/Home_slider";
 
 function Home() {
-	axiosInstance.get(`/user/all-profiles/`).then((res) => {
-		console.log(res)
-		console.log(res.data)
-	})
 
 	return (
-		<div>
-			<Testpage />
-		</div>
+			<HomeSlider/>
+
 	)
 }
 
